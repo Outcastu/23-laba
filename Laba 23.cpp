6678a674aa16c27aa5de44503c89f0290f6de69b
Outcastu <162549533+Outcastu@users.noreply.github.com>
2025-03-25 23:56:17 +0400

    Add files via upload

--- Laba 23.cpp
+++ Laba 23.cpp
@@ -1,120 +1,138 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <Windows.h>
+#include <string.h>
 
 #define MAX_LEN 80
-char s[MAX_LEN];
+    char s[MAX_LEN];
 
+    void ex1() {
+        printf("Калинкин Андрей\n");
+        printf("ЛР23 Задача 1\n");
 
+        // Открываем файл для чтения и записи
+        FILE* fin = fopen("E:\\ОАиП\\текстовые файлы\\laba23\\text1.txt", "r+");
+        if (fin == NULL) {
+            printf("Входной файл не найден");
+            return;
+        }
 
-void ex1() {
-	printf("Калинкин Андрей\n");
-	printf("ЛР23 Задача 1\n");
-	// Входной файл
-	FILE* fin = fopen("E:\\уник\\ОА и П\\Labs\\laba23\\text1.txt", "rt");
-	if (fin == NULL) {
-		printf("Входной файл не найден");
-		return;
-	}
-	// Выходной файл
-	FILE* fout;
-	fout = fopen("E:\\уник\\ОА и П\\Labs\\laba23\\out1.txt", "wt");
-	if (fout == NULL) {
-		printf("Выходной файл не создался");
-		return;
-	}
-	// в цикле для всех строк
-	while (!feof(fin)) {
-		// загрузить строку
-		if (fgets(s, MAX_LEN - 1, fin) != NULL) {
-			// обработать загруженную строку
-			for (int i = 0; s[i] != '\0'; i++) {
-				if (s[i] == '\t') // Если прочитан пробел
-					s[i] = '%'; // Заменяем его на '%’
-			}
-			// сохранить строку в выходной файл
-			fprintf(fout, "%s", s);
-			printf(">>%s<<\n", s);
-		}
-	}
-	// закрыли файлы
-	fclose(fin);
-	fclose(fout);
-	printf("ЛР23 Задача 1 FINISH\n");
-}
-void ex2() {
-	printf("Задача 2 \n");
-	// Входной файл
-	FILE* fin = fopen("E:\\уник\\ОА и П\\Labs\\laba23\\text1.txt", "rt");
-	if (fin == NULL) {
-		printf("Входной файл не найден");
-		return;
-	}
-	// Выходной файл
-	FILE* fout;
-	fout = fopen("E:\\уник\\ОА и П\\Labs\\laba23\\out2.txt", "wt");
-	if (fout == NULL) {
-		printf("Выходной файл не создался");
-		return;
-	}
-	// в цикле для всех строк
-	while (!feof(fin)) {
-		// загрузить строку
-		if (fgets(s, MAX_LEN - 1, fin) != NULL) {
-			// обработать загруженную строку
-			for (int i = 0; s[i] != '\0'; i++) {
-				s[i] = toupper(s[i]);
-			}
-			// сохранить строку в выходной файл
-			fprintf(fout, "%s", s);
-			printf(">>%s\n", s);
-		}
-	}
-	// закрыли файлы
-	fclose(fin);
-	fclose(fout);
-	printf("ЛР23 Задача 2 FINISH\n");
+        // Создаем временный файл для записи
+        FILE* fout = fopen("E:\\ОАиП\\текстовые файлы\\laba23\\temp.txt", "wt");
+        if (fout == NULL) {
+            printf("Временный файл не создался");
+            fclose(fin);
+            return;
+        }
 
-}
-void ex3() {
-	int k = 0;
-	int i;
-	// Входной файл
-	FILE* fin = fopen("E:\\уник\\ОА и П\\Labs\\laba23\\Text1.txt", "rt");
-	if (fin == NULL) {
-		printf("Входной файл не найден");
-		return;
-	}
-	// Выходной файл
-	FILE* fout;
-	fout = fopen("E:\\уник\\ОА и П\\Labs\\laba23\\out3.txt", "wt");
-	if (fout == NULL) {
-		printf("Выходной файл не создался");
-		return;
-	}
-	while (!feof(fin)) {
-		// загрузить строку
-		char arr[] = { "#" };
-		if (fgets(s, MAX_LEN - 1, fin) != NULL) {
-			// обработать загруженную строку
-			for (int i = 0; s[i] != '\0'; i++) {
-				if (s[i + 1] == '\0') {
-					s[i] = '#';
-				}
-			}
-			// сохранить строку в выходной файл
-			fprintf(fout, "%s", s);
-			printf(">>%s\n", s);
-		}
-	}
-	// закрыли файлы
-	fclose(fin);
-	fclose(fout);
-	printf("ЛР23 Задача 3 FINISH\n");
+        // В цикле для всех строк
+        while (fgets(s, MAX_LEN - 1, fin) != NULL) {
+            // Обработать загруженную строку
+            for (int i = 0; s[i] != '\0'; i++) {
+                if (s[i] == '\t') // Если прочитан табулятор
+                    s[i] = '%'; // Заменяем его на '%'
+            }
+            // Сохранить строку во временный файл с добавлением новой строки
+            fprintf(fout, "%s", s); // Здесь уже есть символ новой строки в конце строки s
+            printf(">>%s", s); // Убираем \n, чтобы не было лишнего перехода
+        }
 
-}
-void main() {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	ex3();
-}
+        // Закрываем файлы
+        fclose(fin);
+        fclose(fout);
+
+        // Заменяем оригинальный файл временным
+        remove("E:\\ОАиП\\текстовые файлы\\laba23\\text1.txt");
+        rename("E:\\ОАиП\\текстовые файлы\\laba23\\temp.txt", "E:\\ОАиП\\текстовые файлы\\laba23\\text1.txt");
+
+        printf("ЛР23 Задача 1 FINISH\n");
+    }
+
+    void ex2() {
+        printf("Задача 2 \n");
+
+        // Открываем файл для чтения и записи
+        FILE* fin = fopen("E:\\ОАиП\\текстовые файлы\\laba23\\text1.txt", "r+");
+        if (fin == NULL) {
+            printf("Входной файл не найден");
+            return;
+        }
+
+        // Создаем временный файл для записи
+        FILE* fout = fopen("E:\\ОАиП\\текстовые файлы\\laba23\\temp.txt", "wt");
+        if (fout == NULL) {
+            printf("Временный файл не создался");
+            fclose(fin);
+            return;
+        }
+
+        // В цикле для всех строк
+        while (fgets(s, MAX_LEN - 1, fin) != NULL) {
+            // Обработать загруженную строку
+            for (int i = 0; s[i] != '\0'; i++) {
+                s[i] = toupper(s[i]);
+            }
+            // Сохранить строку во временный файл с добавлением новой строки
+            fprintf(fout, "%s", s); // Здесь уже есть символ новой строки в конце строки s
+            printf(">>%s", s); // Убираем \n, чтобы не было лишнего перехода
+        }
+
+        // Закрываем файлы
+        fclose(fin);
+        fclose(fout);
+
+        // Заменяем оригинальный файл временным
+        remove("E:\\ОАиП\\текстовые файлы\\laba23\\text1.txt");
+        rename("E:\\ОАиП\\текстовые файлы\\laba23\\temp.txt", "E:\\ОАиП\\текстовые файлы\\laba23\\text1.txt");
+
+        printf("ЛР23 Задача 2 FINISH\n");
+    }
+
+    void ex3() {
+        printf("Задача 3 \n");
+
+        // Открываем файл для чтения
+        FILE* fin = fopen("E:\\ОАиП\\текстовые файлы\\laba23\\text1.txt", "r");
+        if (fin == NULL) {
+            printf("Входной файл не найден");
+            return;
+        }
+
+        // Создаем временный файл для записи
+        FILE* fout = fopen("E:\\ОАиП\\текстовые файлы\\laba23\\temp.txt", "wt");
+        if (fout == NULL) {
+            printf("Временный файл не создался");
+            fclose(fin);
+            return;
+        }
+
+        // В цикле для всех строк
+        while (fgets(s, MAX_LEN - 1, fin) != NULL) {
+            // Убираем символ новой строки, если он есть
+            s[strcspn(s, "\n")] = 0; // Удаляем символ новой строки, если он есть
+
+            // Добавляем '#' в конец строки
+            strcat(s, "#");
+
+            // Сохранить строку во временный файл с добавлением новой строки
+            fprintf(fout, "%s\n", s); // Записываем строку с новой строки
+        }
+
+        // Закрываем файлы
+        fclose(fin);
+        fclose(fout);
+
+        // Заменяем оригинальный файл временным
+        remove("E:\\ОАиП\\текстовые файлы\\laba23\\text1.txt");
+        rename("E:\\ОАиП\\текстовые файлы\\laba23\\temp.txt", "E:\\ОАиП\\текстовые файлы\\laba23\\text1.txt");
+
+        printf("ЛР23 Задача 3 FINISH\n");
+    }
+    void main() {
+        SetConsoleCP(1251);
+        SetConsoleOutputCP(1251);
+        ex1();
+        ex2();
+        ex3();
+    }
+    
